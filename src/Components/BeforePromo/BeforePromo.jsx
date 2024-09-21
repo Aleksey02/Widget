9469985b1a72a523524ddff8bee4b4c10492f101
Aleksey02 <lyosha2002stich@gmail.com>
2024-09-21 14:21:19 +0500

    feat: Add timer

--- src/Components/BeforePromo/BeforePromo.jsx
+++ src/Components/BeforePromo/BeforePromo.jsx
@@ -1,12 +1,34 @@
 import './BeforePromo.scss'
 import { useEffect, useState } from 'react'
 
-function BeforePromo(){
+function getTime(){
+	const now = new Date();
+	const target = new Date(Date.now() + 24 * 60 * 60 * 1000);
+	target.setHours(0);
+	target.setMinutes(0);
+	target.setSeconds(0);
+	target.setMilliseconds(0);
+	const diff = target - now;
+	const hours = Math.floor(diff / 1000 / 60 / 60);
+	const minutes = Math.floor((diff / 1000 / 60) % 60);
+	const seconds = Math.floor((diff / 1000 ) % 60 );
+	
+	return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`
+	
+}
 
+function BeforePromo(){
+	const [time, setTime] = useState(getTime())
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTime(getTime());
+		}, 1000)
+		return () => clearInterval(interval)
+	}, [])
 	return (
 		<div className="beforePromo">
 			<p className="beforePromo__text">Начало через:</p>
-			<p className="beforePromo__text">19:50:38</p>
+			<p className="beforePromo__text">{time}</p>
 		</div>
 	)
 }
--- src/Components/BeginPromo/BeginPromo.jsx
+++ src/Components/BeginPromo/BeginPromo.jsx
@@ -1,6 +1,30 @@
 import './BeginPromo.scss'
+import { useEffect, useState } from 'react';
+
+function getTime(){
+	const now = new Date();
+	const target = new Date(Date.now() + 24 * 60 * 60 * 1000);
+	target.setHours(0);
+	target.setMinutes(0);
+	target.setSeconds(0);
+	target.setMilliseconds(0);
+	const diff = target - now;
+	const hours = Math.floor(diff / 1000 / 60 / 60);
+	const minutes = Math.floor((diff / 1000 / 60) % 60);
+	const seconds = Math.floor((diff / 1000 ) % 60 );
+	
+	return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`
+	
+}
 
 function BeginPromo(){
+	const [time, setTime] = useState(getTime())
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTime(getTime());
+		}, 1000)
+		return () => clearInterval(interval)
+	}, [])
 	return (
 		<div className="beginPromo">
 			<div className="beginPromo__flex">
@@ -9,7 +33,7 @@ function BeginPromo(){
 			</div>
 			<div className="beginPromo__flex">
 				<p className="beginPromo__text">Сброс очков:</p>
-				<p className="beginPromo__text">02:37:07</p>
+				<p className="beginPromo__text">{time}</p>
 			</div>
 		</div>
 	)
