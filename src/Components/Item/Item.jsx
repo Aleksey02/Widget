97c728dc15ee1c8faec8187530e807c044cabf48
Aleksey02 <lyosha2002stich@gmail.com>
2024-09-21 15:26:50 +0500

    feat: Add icon game

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,21 +1,22 @@
 import './Item.scss'
 import question from '../../assets/question.svg'
 import arrow from '../../assets/arrow_down.svg'
-import imgFull from '../../assets/icon_game_full.png'
-import imgPart from '../../assets/icon_game_part.png'
-import { Children, useState } from 'react'
+
+import img from '../../assets/icon_game_base.png'
+import { useState } from 'react'
 
 function Item({title, SubMenu, children}) {
 	const [open, setOpen] = useState(false)
-	
+	const imageColor = (title === 'До начала промо' || title === 'Акция завершена') ? 'rgb(247 65 32) 10%, #8B0000 100%' : 'rgb(205 21 21) 32%';
 	return (
 		<div className="item">
 			<div className="item__title">{title}</div>
 			<div className="item__wrapper">
 				<div className="item__box">
 					<div className="item__block">
-						{ (title == 'beforePromo' || title == 'endPromo') && <img src={imgFull} alt="logo game" /> }
-						{ !(title == 'beforePromo' || title == 'endPromo') && <img src={imgPart} alt="logo game" /> }
+						<div className="item__img" style={{background: `linear-gradient(to top, ${imageColor}, #27292e 0%)`}}>
+							<img src={img} alt="logo game" /> 
+						</div>
 						<h3 className="item__subtitle">Compenigma August 2020</h3>
 					</div>
 					<div className="item__block">
